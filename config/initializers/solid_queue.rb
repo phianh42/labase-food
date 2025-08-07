2a266fc67df9b846364f3ce4ba875fa77e361fbd
Phi-Anh Nguyen <phianh.n@gmail.com>
2025-08-07 16:23:55 +0200

    switched to inline queue

--- config/initializers/solid_queue.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/solid_queue.rb
-if Rails.env.production?
-  Rails.application.configure do
-    # Force solid_queue to use main database connection
-    config.solid_queue.connects_to = { database: { writing: :primary } }
-  end
-end
